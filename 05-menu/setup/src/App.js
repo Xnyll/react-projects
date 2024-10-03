305a213754f9a0f3d8e5f482d513ca7630eb2d1b
Xnyll <47606240+Xnyll@users.noreply.github.com>
2024-10-03 17:51:41 +0100

    Menu

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -3,8 +3,35 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+//array using spread operator to include the "all" category/button as well as everything else in an array
+const allCategories = ['all', ...new Set(items.map((item) => item.category))] //Set to get only unique values from categories
+
+
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if(category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category)
+    setMenuItems(newItems)
+  };
+
+  return <main>
+    <section className='menu section'>
+      <div className='title'>
+        <h2>Our Menu</h2>
+        <div className='underline'></div>
+      </div>
+      <Categories categories={categories} filterItems={filterItems}/>
+      <Menu items={menuItems}/>
+    </section>
+  </main>;
 }
 
 export default App;
+
+
